7a35ea0e62239bafaaf1dfead36bc5623a86aec5
mayday1004 <iko7695@gmail.com>
2022-11-30 20:36:55 +0800

    fix

--- utils/error.js
+++ utils/error.js
@@ -1,3 +1,5 @@
-export const catchErr = err => {
+const catchErr = err => {
   return err.response?.data?.message ? err.response.data.message : err.message ? err.message : err;
 };
+
+export default catchErr;
